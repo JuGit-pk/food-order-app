98efb453447fb7aefee958c3e9280a2f4b84e9fa
Muhammad Junaid Iqbal <cuilahore147@gmail.com>
2021-08-14 17:39:07 +0500

    add cart bumping animation

--- src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,15 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import CartContext from "../../../store/cart-context";
 import CartIcon from "../../Cart/CartIcon";
 import "./HeaderCartButton.css";
 
 const HeaderCartButton = (props) => {
+  //FOR BUMING ANIMATION
+  const [bumpingBtn, setBumpingBtn] = useState(false);
   const cartCtx = useContext(CartContext);
+  const buttonClass = `button ${bumpingBtn && "bump"}`;
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBumpingBtn(true);
+
+    const timer = setTimeout(() => {
+      setBumpingBtn(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
   const numberOfItemsForBadge = cartCtx.items.reduce((current, item) => {
     return current + item.amount;
   }, 0);
   return (
-    <div className="button" onClick={props.onClick}>
+    <div className={buttonClass} onClick={props.onClick}>
       <span className="icon">
         <CartIcon />
       </span>
